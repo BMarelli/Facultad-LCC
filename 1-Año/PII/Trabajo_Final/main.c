2c5a6399921635f47f5a600ca00aa4e3212aedbe
BMarelli <marellibautista@gmail.com>
2020-07-02 15:23:22 -0300

    first commit

---
+++ 1-Año/PII/Trabajo_Final/main.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// MODO DE USO:
+// El usuario tiene que introducir el nombre del archivo, de preferencia (laberinto.txt)
+// Se generara un archivo llamado maze.txt, en el cual se encuentra el laberinto
+
+// El archivo a recibir (laberinto.txt) debe cumplir con ciertas características:
+// - En la primera linea tiene que estar el tamaño que tendra el laberinto.
+//   Primero es la cantidad de filas y luego la cantidad de columnas, separados por un espacio.
+// - Luego, la segunda linea tiene que tener las coordenadas del objetivo, separados por un espacio.
+// - Por ultimo, las lineas restantes representan las coordenadas de los muros
+//   Cada elemento tiene que estar separado por espacios
+// - Importante! El archivo debe tener como maximo 22 lines:
+//   1 (tamaño) + 1 (objetivo) + 20 (muros)
+// - Importante! El archivo debe terminar con un salto de linea
+// Ejemplo: (archivo: laberinto.txt)
+/*
+    6 6
+    5 4
+    1 2
+    1 4
+    1 5
+    2 0
+    2 1
+    2 2
+    2 4
+    2 5
+    3 0
+    3 1
+    3 4
+    3 5
+    4 0
+    4 1
+    4 3
+    4 4
+    4 5
+    5 0
+    5 1
+    5 5
+
+*/
+
+// Salida: (archivo: maze.txt)
+/*
+    0 0 0 0 0 0
+    0 1 0 0 1 1
+    1 1 1 0 1 1
+    1 1 0 0 1 1
+    1 1 0 1 1 1
+    1 1 0 0 2 1
+
+*/
+
+
+// parseFile: char[] int[2][22] -> int
+// Esta funcion recibe el nombre del archivo introducido por el usuario y una matriz de 2 filas y 22 columnas
+// Esta matriz contiene las coordenadas que se encuentran en el archivo recibido.
+// Aclaracion: El tamaño de esta matriz es de 22 ya que sabemos que el maximo de muros que podemos recibir es 20, luego
+// contamos tambien el tamaño del laberinto y las coordenadas del objetivo.
+// 22 = 1 (tamaño) + 1 (objetivo) + 20 (muros)
+// Por ultimo, la matriz coordinates tiene 2 filas porque cada fila representa cada elemento de las coordenadas
+// En el caso de que el archivo no exista, devuelve un error
+// En el caso contrario, devuelve la cantidad de lineas leidas
+int parseFile(char filename[], int coordinates[2][22]){
+    FILE * file;
+    file = fopen(filename, "r");
+
+    if(file == NULL) {
+        printf("Error: El archivo %s no existe!\n", filename);
+
+        return -1;
+    }
+
+    int row, col;
+    int i = 0;
+
+    while(fscanf(file, "%d %d\n", &row, &col) != EOF && i < 22) {
+        coordinates[0][i] = row;
+        coordinates[1][i] = col;
+        i++;
+    }
+
+    fclose(file);
+
+    return i;
+}
+
+
+// isMazeValid: int[2][22] int -> int
+// Recibe una matriz con las coordenadas obtenidas del archivo y la cantidad de coordenadas leidas
+// Devuelve 1 si las coordenadas son validas y 0 de lo contrario
+int isMazeValid(int coordinates[2][22], int numCordinates) {
+    int numRow = coordinates[0][0];
+    int numCol = coordinates[1][0];
+    int rowObjective = coordinates[0][1];
+    int colObjective = coordinates[1][1];
+    int wrongWalls = 0;
+    int index = 2;
+
+    if(numRow <= 0 || numCol <= 0) return 0;
+
+    if(rowObjective < 0 || colObjective < 0 || rowObjective >= numRow || colObjective >= numCol) return 0;
+
+    while (index < numCordinates) {
+        if(coordinates[0][index] < 0 || coordinates[0][index] >= numRow ||
+           coordinates[1][index] < 0 || coordinates[1][index] >= numRow) {
+            wrongWalls++;
+        }
+
+        index++;
+    }
+
+    if(wrongWalls) return 0;
+
+    return 1;
+
+}
+
+
+// buildMaze: int[15][15] int[2][22] int -> void
+// Esta funcion recibe una matriz que representa el laberinto, una matriz de coordenadas y la cantidad de coordenadas
+// Segun las coordenadas recibidas, las cuales son del muro y del objetivo, modifica el laberinto
+void buildMaze(int maze[15][15], int coordinates[2][22], int numCordinates) {
+    int numRow = coordinates[0][0];
+    int numCol = coordinates[0][1];
+    int a = 1;
+
+    maze[coordinates[0][1]][coordinates[1][1]] = 2; // introducimos el objetivo en su coordenada
+
+    for (int i = 2; i < numCordinates; i++) {
+        maze[coordinates[0][i]][coordinates[1][i]] = 1; // introducimos los muros en sus coordenadas
+    }
+}
+
+
+// writeMaze: char[] int[15][15] int[2][22] -> void
+// Recibe el nombre del archivo de salida, la matriz del laberinto y las coordenadas obtenidas del archivo leido
+// Crea un nuevo archivo llamado segun el valor de outFilename. En el caso de que ya existe, lo reescribe sobre este
+// Escribre, separado por espacios cada valor que tenga la matriz del laberinto
+void writeMaze(char outFilename[], int maze[15][15], int coordinates[2][22]) {
+    FILE * file;
+    file = fopen(outFilename, "w+");  // creamos o sobreescribimos en el archivo maze.txt
+
+    int numRow = coordinates[0][0];
+    int numCol = coordinates[1][0];
+
+    for (int i = 0; i < numRow; i++) {
+        for (int j = 0; j < numCol; j++) {
+            fprintf(file, "%d", maze[i][j]);
+            
+            if (j != numCol - 1) fprintf(file, " ");
+        }
+
+        fprintf(file, "\n");
+    }
+
+    printf("Se a creado el archivo: %s con el laberinto!\n", outFilename);
+    
+    fclose(file);
+}
+
+
+// runAssert: void
+// Corre los asserts
+void runAssert() {
+    int coordinates[2][22] = {{4, 3, 1, 2}, {4, 3, 1, 2}};
+    int coordinates2[2][22] = {{4, 5, 1, 2}, {4, 3, 1, 2}};
+    int numCordinates = 4;
+
+    assert(isMazeValid(coordinates, 4) == 1);
+    assert(isMazeValid(coordinates2, 4) == 0);
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc <= 3) {
+        runAssert();  // corremos los asserts
+
+        // char outFilename[] = (argc == 3) ? argv[2] : "maze.txt";
+        int coordinates[2][22];  // las columnas representan coordenadas
+        int maze[15][15] = {0};  // lo iniciamos con 0 ya que luego lo modificaremos segun el archivo
+
+
+        int numCordinates = parseFile(argv[1], coordinates);
+
+        if (numCordinates == -1) return -1;
+
+        if (!isMazeValid(coordinates, numCordinates)){
+            printf("Se produjo un error leyendo el archivo: %s\n", argv[1]);
+            printf("Primero revise la documentacion sobre como ingresar el archivo\n");
+            return -1;
+        }
+
+        buildMaze(maze, coordinates, numCordinates);
+        writeMaze(argv[2], maze, coordinates);
+        // writeMaze(outFilename, maze, coordinates);
+
+        return 0;
+    } 
+    else {
+        printf("La cantidad de argumentos es incorrecta!\n");
+        printf("MODO DE USO: ./main [entrada.txt] {salida.txt}\n");
+        return -1;
+    }
+}
---
+++ 1-Año/PII/Trabajo_Final/main.py
@@ -0,0 +1,221 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+from termcolor import colored
+import os
+import sys
+
+# Representamos a un maze como una list(list(str)):
+# maze: list(list(str))
+# Por ejemplo:
+# [["0", "0", "0", "0", "0", "0"],
+#  ["1", "1", "1", "0", "1", "1"],
+#  ["1", "1", "1", "0", "1", "1"],
+#  ["1", "1", "0", "0", "1", "1"],
+#  ["1", "1", "0", "1", "1", "1"],
+#  ["1", "1", "0", "0", "2", "1"]]
+
+# Llamamos a colored(maze) al maze que fue editado con la libreria termcolor
+
+# El archivo con el laberinto tiene que tener la siguiente forma:
+# - Cada linea tiene que ser una fila del laberinto
+# - Representamos con "1" las paredes
+# - Representamos con "0" los espacios en blanco
+# - Representamos con "-" los lugares visitados
+# - Representamos el objetivo con "2"
+# - Cada valor (int) tienen que estar separados con un espacio
+# - Importante! El archivo debe finalizar con un salto de línea
+# Ejemplo:
+
+# 0 1 0 0 0 1 0 0 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 1 0 1 0 1 0 1 0 1
+# 0 0 0 1 0 0 0 1 0 2
+#
+
+
+# parse_maze: file -> maze
+# Recibe un archivo que contiene el maze, el cual tiene que cumplir las
+# caracteristicas descriptas
+# Devuelve el maze que esta representado en el archivo
+def parse_maze(f):
+    maze = [line.strip().split(" ") for line in f.readlines()]
+
+    return maze
+
+
+# prefer_objective: maze list(tuple(int, int)) -> list(tuple(int, int))
+# Recibe un maze y una lista de tupla de numeros
+# Esta lista representa una lista de coordenadas las cuales son los lugares que
+# podemos visitar
+# Devuelve una lista de tuplas
+# En el caso de que el objetivo este en unos de esos espacios que podemos
+# visitar, devuelve una lista con esa coordenada
+# De lo contrario devuelve la lista recibida anteriormente
+def prefer_objective(maze, free_spaces):
+    spaces = []
+
+    for space in free_spaces:
+        if (maze[space[0]][space[1]] == "2"):
+            spaces.append(space)
+
+    if len(spaces) == 0:
+        return free_spaces
+
+    return spaces
+
+
+# possible_actions: maze tuple(int, int) -> list(tuple(int, int))
+# Recibe un maze y una tupla de numeros
+# Los elementos de la tuplas representan la fila y la columna dentro del
+# laberinto
+# Devuelve una lista de tuplas que representan las coordenadas a la cuales
+# nos podemos mover
+# Tenemos como prioridad devolver primero la posibilidades de mover hacia
+# abajo o hacia la derecha. Luego consideramos a para arriba y la izquierda
+def possible_actions(maze, position):
+    free_spaces = []
+    n_rows = len(maze)
+    n_cols = len(maze[0])
+
+    if (position[0] != n_rows - 1 and
+        maze[position[0] + 1][position[1]] != "1" and
+            maze[position[0] + 1][position[1]] != "-"):
+        free_spaces.append((position[0] + 1, position[1]))
+
+    if (position[1] != n_cols - 1 and
+        maze[position[0]][position[1] + 1] != "1" and
+            maze[position[0]][position[1] + 1] != "-"):
+        free_spaces.append((position[0], position[1] + 1))
+
+    if (position[0] != 0 and
+        maze[position[0] - 1][position[1]] != "1" and
+            maze[position[0] - 1][position[1]] != "-"):
+        free_spaces.append((position[0] - 1, position[1]))
+
+    if (position[1] != 0 and
+        maze[position[0]][position[1] - 1] != "1" and
+            maze[position[0]][position[1] - 1] != "-"):
+        free_spaces.append((position[0], position[1] - 1))
+
+    return prefer_objective(maze, free_spaces)
+
+
+# color_maze: maze list(tuple(int, int)) -> colored(maze)
+# Recibe una laberinto y una lista de tuplas
+# Las tuplas representan las coordenadas que forman la solucion del laberinto
+# Esta funcion cambia el color de los elementos que se encuentran en las
+# coordenadas que se encuentran en la lista de tuplas
+# Devuelve un colored(maze), para que sea mas facil visualizar el camino
+# Representamos con "+" (verde) al camino resuelve el laberinto
+# Representamos con "-" (blanco) a los espacios que visitamos y
+# no llegan al objetivo
+# Representamos con "1" (rojo) a las paredes
+# Representamos con " " a los espacios que no visitamos
+def color_maze(maze, steps=[]):
+    for row in range(len(maze)):
+        for col in range(len(maze[0])):
+            if ((row, col) in steps):
+                if (maze[row][col] == "2"):
+                    maze[row][col] = colored("+", color="yellow")
+
+                else:
+                    maze[row][col] = colored("+", color="green")
+
+            elif (maze[row][col] == "-"):
+                maze[row][col] = colored(maze[row][col], color="blue")
+
+            elif (maze[row][col] == "2"):
+                maze[row][col] = colored(maze[row][col], color="yellow")
+
+            elif (maze[row][col] == "0"):
+                maze[row][col] = maze[row][col] = " "
+
+            else:
+                maze[row][col] = colored("1", color="red")
+
+    return maze
+
+
+# display_maze: maze bool list(tuple(int, int)) -> None
+# Recibe un laberinto y el camino hacia la salida y lo muestra
+# En el caso que visual sea True, colorea el camino
+def display_maze(maze, steps=[]):
+    maze = color_maze(maze, steps)
+
+    print("")
+    print("\n".join([" ".join(row) for row in maze]))
+
+
+# solve_maze: maze -> list(tuple(int, int))
+# Recibe un maze
+# Devuelve una lista con tuplas que representan coordenadas
+# Esta lista contiene las coordenadas espesificas que resuelven el laberinto
+# Para poder resolver el maze y no entrar en  lugares visitados, cambia los
+# "0" que visito por "-"
+def solve_maze(maze):
+    is_solve = False
+    current_position = (0, 0)
+    steps = []
+
+    while not is_solve:
+        free_spaces = possible_actions(maze, current_position)
+        n_actions = len(free_spaces)
+
+        if len(steps) == 0 and n_actions == 0:
+            print(colored("No se pudo resolver el laberinto!", color="red"))
+            is_solve = True
+
+        if (n_actions == 0):
+            maze[current_position[0]][current_position[1]] = "-"
+            if (len(steps) != 0):
+                current_position = steps.pop()
+
+        else:
+            maze[current_position[0]][current_position[1]] = "-"
+            steps.append(current_position)
+            current_position = free_spaces[0]
+
+            element_in_maze = maze[current_position[0]][current_position[1]]
+
+            if element_in_maze == "2":
+                steps.append(current_position)
+                print(colored("Se pudo resolver el laberinto!", color="green"))
+                is_solve = True
+
+    return steps
+
+
+if __name__ == "__main__":
+    done = False
+    if (len(sys.argv) == 3):
+        print("Trabajo Practico Final")
+        print("Alumno: Bautista Marelli\n")
+
+        filename = str(sys.argv[1])
+        VISUAL = sys.argv[2] == "1"
+        try:
+            with open(filename, "r") as f:
+                maze = parse_maze(f)
+
+                input(colored(">>> Enter para continuar", color="green"))
+                # borramos la pantalla
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+                steps = solve_maze(maze)
+                print(steps)
+
+                if VISUAL:
+                    display_maze(maze, steps)
+
+        except FileNotFoundError:
+            print(colored("El archivo no existe", color="red"))
+    else:
+        print(colored("La cantidad de argumentos es incorrecta!", color="red"))
+        print(colored("MODO DE USO: python main.py [entrada.txt] [visual]",
+                      color="red"))
